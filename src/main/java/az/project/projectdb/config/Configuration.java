ce7740e76f6daf16302cf6ee13f33e366135159d
Farid <98278238+faridfarajli@users.noreply.github.com>
2024-06-04 10:29:54 +0400

    Update Configuration.java

--- src/main/java/az/project/projectdb/config/Configuration.java
+++ src/main/java/az/project/projectdb/config/Configuration.java
@@ -14,9 +14,9 @@ public class Configuration {
         public DataSource dataSource() {
             DriverManagerDataSource dataSource = new DriverManagerDataSource();
             dataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-            dataSource.setUrl("jdbc:oracle:thin:@10.81.1.33:1521:ORCL");
-            dataSource.setUsername("SYSTEM");
-            dataSource.setPassword("mark1q2w3e$$*");
+            dataSource.setUrl("jdbc:oracle:thin:@");
+            dataSource.setUsername("");
+            dataSource.setPassword("");
             return dataSource;
         }
 
